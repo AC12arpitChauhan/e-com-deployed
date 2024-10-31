9d9383eb66fc693169b8fa300d18427acd447d3b
AC12arpitChauhan <156501624+AC12arpitChauhan@users.noreply.github.com>
2024-10-31 07:28:40 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Experience the ultimate in online shopping with our e-commerce
+          platform, where style meets convenience. Browse our extensive
+          collection of garments, including trendy tops, chic dresses, cozy
+          sweaters, and versatile bottoms for all occasions. Our user-friendly
+          website makes it easy to find the perfect outfit, with detailed
+          product descriptions, high-quality images, and size guides to ensure
+          the perfect fit. Enjoy seamless navigation, secure checkout, and fast
+          shipping right to your doorstep. With new arrivals added regularly,
+          stay ahead of the fashion curve. Shop with confidence and elevate your
+          wardrobe effortlessly at our one-stop fashion destination.
+        </p>
+        <p>
+          Shop the latest fashion effortlessly on our e-commerce site. Explore a
+          wide range of trendy garments, from chic dresses to cozy sweaters.
+          Enjoy detailed descriptions, high-quality images, and easy navigation.
+          Secure checkout and fast shipping ensure a seamless shopping
+          experience. Stay stylish with our regularly updated collections.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
